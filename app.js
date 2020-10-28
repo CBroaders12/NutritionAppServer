9ba4fc1404db0b8c2a62a04b1409a49bdebace7b
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-10-28 10:57:06 -0400

    scaffold files and add scripts

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const Express = require('Express');
+const app = Express();
+const port = 5200;
+
+
+//TODO: Import Controllers
+
+//TODO: CORS Middleware
+
+app.use(Express.json());
+
+//Controller Routes
+//Open Routes
+//TODO: /user route
+
+//Authenticated Routes
+//TODO: /log route
+
+app.listen(port, () => console.log(`[server]: Listening on http://localhost:${port}`));
