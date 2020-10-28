ce11987f79096b577044a4856d842b62a515984d
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-10-28 15:47:45 -0400

    login route

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,7 +6,7 @@ const { User } = require('../models');
 const userController = Router();
 
 /*
-  TODO: register user - '/register'
+  register user - '/register'
   POST Request
 */
 userController.post('/register', async (req, res) => {
@@ -22,6 +22,7 @@ userController.post('/register', async (req, res) => {
       message: 'User registered!'
     });
   } catch (error) {
+    // TODO: Add in specific error when username or password is already registered
       res.status(500).json({
         message: 'Failed to register user'
       });
@@ -29,11 +30,29 @@ userController.post('/register', async (req, res) => {
 });
 
 /*
-  TODO: login user - '/login'
+  login user - '/login'
   POST Request
 */
-userController.post('/login', (req, res) => {
-  res.send('hello from the login route');
+userController.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+  try {
+    let loginUser = await User.findOne( { where: { username } } );
+    if ( loginUser && await bcrypt.compare(password, loginUser.password)) {
+      const token = jwt.sign({ id: loginUser.id }, process.env.JWT_SECRET);
+      res.status(200).json({
+        message: 'Login successful',
+        token
+      });
+    } else {
+      res.status(401).json({
+        message: 'Login failed'
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error logging in'
+    });
+  }
 });
 
 module.exports = userController;
