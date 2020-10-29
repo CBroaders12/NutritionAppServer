410262e0610ab2492a4751830b136de8770b95c2
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2020-10-29 13:18:09 -0400

    move secrets

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = require('./db');
 const app = Express();
 
 // Controllers
-const controllers = require('./controllers')
+const controllers = require('./controllers');
 
 // CORS Middleware
 app.use(require('./middleware/corsMiddleware'));
@@ -19,13 +19,11 @@ app.use(Express.json());
 app.use('/user', controllers.User);
 
 //Authenticated Routes
-//TODO: /log route
 app.use(require('./middleware/validate-session'));
 app.use('/food', controllers.Food);
 
 db.authenticate()
 .then(() => db.sync())
 .then(() => {
-  app.listen(5200, () => console.log(`[server]: Listening on http://localhost:5200`));
-})
-
+  app.listen(process.env.PORT, () => console.log(`[server]: Listening on http://localhost:${process.env.PORT}`));
+});
